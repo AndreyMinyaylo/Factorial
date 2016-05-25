13244581e4dad3124d3924c12ebf07716fdd4020
AndreyMinyaylo <minyailoandrey@gmail.com>
2016-05-25 20:22:25 +0300

    worckProgram

--- Factorial/Factorial/Program.cs
+++ Factorial/Factorial/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace Factorial
 {
-    class Program
+    class factorial
     {
+        public static double input()
+        {
+            Console.WriteLine("Enter number:");
+            double number = int.Parse(Console.ReadLine());
+            return number; 
+        }
+
+        public static void write(double someNumber)
+        {
+            Console.WriteLine("Fact({0}) = {1}", someNumber, fact(someNumber));
+            Console.ReadKey();
+        }
+      
+
+    public static double fact(double someNumber)
+    {
+        return someNumber == 1 ? someNumber : fact(someNumber - 1) * someNumber;
+
+    }
+
+public static void outData()
+        {
+            double someNumber = input();
+            write(someNumber);
+            
+        }
+
         static void Main(string[] args)
         {
+            outData();
         }
     }
 }
